de8e29eb7d538b1712a242dc0f520d075982f73d
birdkron <js8215001@gmail.com>
2024-10-05 11:00:52 -0600

    Agregada consulta para reporte 1

--- Reportes/Consultas Reportes.sql
+++ Reportes/Consultas Reportes.sql
@@ -7,14 +7,33 @@
 USE ProyectosBDD
 GO
 
+--Reporte 1
+
+SELECT
+*
+FROM Proyectos
+
+SELECT
+pr.Nombre as 'Proyecto',
+eq.Nombre as 'Equipo'
+FROM 
+Proyectos pr
+Inner Join Equipos eq on eq.Id = pr.EquipoId
+WHERE DATEDIFF(DAY, pr.FechaDeInicio, '2024-01-15') = 0
+GO
+
+--Reporte 2
 SELECT 
 ta.Descripcion as 'Tarea',
 pr.Nombre as 'Proyecto',
 eq.Nombre as 'Equipo',
-pr.FechaDeFin as 'Inicio',
-pr.FechaDeInicio as 'Fin'
+pr.FechaDeFin as 'Fin',
+pr.FechaDeInicio as 'Inicio',
+et.Nombre as 'Estado'
 FROM 
 Tareas ta 
 Inner Join Proyectos pr on pr.Id = ta.ProyectoId
 Inner Join Equipos eq on eq.Id = pr.EquipoId
+Inner Join EstadoTareas et on et.Id = ta.EstadoId
+ORDER BY et.Id
 GO
